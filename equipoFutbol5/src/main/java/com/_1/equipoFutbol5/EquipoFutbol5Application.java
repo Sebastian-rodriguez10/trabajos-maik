b51c8f9a4657b518d63b1cf08b0e472ce279d9cd
sebastian rodriguez <juanserodriguez711@gmail.com>
2026-06-05 16:34:54 -0500

    feat: firts commit for created web repository in github

---
+++ equipoFutbol5/src/main/java/com/_1/equipoFutbol5/EquipoFutbol5Application.java
@@ -0,0 +1,13 @@
+package com._1.equipoFutbol5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipoFutbol5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EquipoFutbol5Application.class, args);
+	}
+
+}
---
+++ equipoFutbol5/src/test/java/com/_1/equipoFutbol5/EquipoFutbol5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com._1.equipoFutbol5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipoFutbol5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
